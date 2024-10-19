2aa23c45851766016aea73dc97c46877c932fc7e
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-19 18:27:20 -0500

    Added more components

--- pokedex/src/Components/Main.jsx
+++ pokedex/src/Components/Main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 const Main = () => {
   return (
     <div className="container">
-      <div className="left-content"></div>
+      <div className="left-content">
+        <Card />
+      </div>
       <div className="right-content"></div>
     </div>
   );
