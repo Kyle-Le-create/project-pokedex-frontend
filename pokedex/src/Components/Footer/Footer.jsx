b3cb110e2204541bdf99da31612a60d0f5ea17d4
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-21 20:46:08 -0500

    Made adjustment towards my Main components, added Cards, and third party API

---
+++ pokedex/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul className="footer__content">
+        <li className="footer__content-name">Designed by Kyle Le</li>
+        <li className="footer__content-year"> 2024</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
